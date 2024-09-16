d788470ea304210b6cbc67fd52af490dc1526efb
Rebeka Knight <rebeka_knight@hotmail.co.uk>
2024-09-16 15:24:55 +0000

    added routes to routes.py and created html files for routes to link to

--- plantspire/routes.py
+++ plantspire/routes.py
@@ -6,3 +6,47 @@ from plantspire.models import Recipes, Categories
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/")
+def get_recipes():
+    return render_template("recipes.html")
+
+
+@app.route("/")
+def add_recipes():
+    return render_template("add-recipes.html")
+
+
+@app.route("/")
+def get_categories():
+    return render_template("categories.html")
+
+
+@app.route("/")
+def add_category ():
+    return render_template("add-category.html")
+
+
+@app.route("/")
+def profile ():
+    return render_template("profile.html")
+
+
+@app.route("/")
+def login ():
+    return render_template("login.html")
+
+
+@app.route("/")
+def logout ():
+    return render_template("logout.html")
+
+
+@app.route("/")
+def register ():
+    return render_template("register.html")
+
+
+
+
