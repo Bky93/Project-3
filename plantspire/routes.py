6d34bbef210ca30770ead6ec2cb747ee7223bcfd
Rebeka Knight <rebeka_knight@hotmail.co.uk>
2024-09-17 07:05:54 +0000

    added content to recipes.html

--- plantspire/routes.py
+++ plantspire/routes.py
@@ -1,49 +1,49 @@
-from flask import render_template
+from flask import Flask, render_template
 from plantspire import app, db
 from plantspire.models import Recipes, Categories
 
 
-@app.route("/")
+@app.route("/home")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 
-@app.route("/")
+@app.route("/get_recipes")
 def get_recipes():
     return render_template("recipes.html")
 
 
-@app.route("/")
+@app.route("/add_recipes")
 def add_recipes():
     return render_template("add-recipes.html")
 
 
-@app.route("/")
+@app.route("/get_categories")
 def get_categories():
     return render_template("categories.html")
 
 
-@app.route("/")
+@app.route("/add_category")
 def add_category ():
     return render_template("add-category.html")
 
 
-@app.route("/")
+@app.route("/profile")
 def profile ():
     return render_template("profile.html")
 
 
-@app.route("/")
+@app.route("/login")
 def login ():
     return render_template("login.html")
 
 
-@app.route("/")
+@app.route("/logout")
 def logout ():
     return render_template("logout.html")
 
 
-@app.route("/")
+@app.route("/register")
 def register ():
     return render_template("register.html")
 
--- plantspire/static/js/script.js
+++ plantspire/static/js/script.js
@@ -2,3 +2,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // initialize modal
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+        dismissible: true, // Modal can be dismissed by clicking outside of it
+        onCloseEnd: function(modal) {
+            // Automatically close modal on another open
+            // prevents multiple modals from opening
+            if (document.getElementsByClassName('modal open').length > 0) {
+                var openedModal = document.getElementsByClassName('modal open')[0];
+                var instance = M.Modal.getInstance(openedModal);
+                instance.close();
+            }
+        }
+    });
+});
